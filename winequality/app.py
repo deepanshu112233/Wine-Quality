d05135d47f9d9f1466c48f3e030bb91911f5c1ba
deepanshu112233 <deepanshusingh2608@gmail.com>
2023-08-30 19:04:02 +0530

    update

--- winequality/app.py
+++ winequality/app.py
@@ -16,17 +16,22 @@ def home():
 @app.route('/',methods=['POST'])
 def predict():
     if request.method == 'POST':
-        fixed_acidity         = float(request.form['fixed_acidity'])
-        volatile_acidity      = float(request.form['volatile_acidity'])
-        citric_acid           = float(request.form['citric_acid'])
-        residual_sugar        = float(request.form['residual_sugar'])
-        chlorides             = float(request.form['chlorides'])
-        free_sulphur_dioxide  = float(request.form['free_sulphur_dioxide'])
-        total_sulphur_dioxide = float(request.form['total_sulphur_dioxide'])
-        density               = float(request.form['density'])
-        ph                    = float(request.form['ph'])
-        sulphates             = float(request.form['sulphates'])
-        alcohol               = float(request.form['alcohol'])
+        try:
+            fixed_acidity         = float(request.form['fixed_acidity'])
+            volatile_acidity      = float(request.form['volatile_acidity'])
+            citric_acid           = float(request.form['citric_acid'])
+            residual_sugar        = float(request.form['residual_sugar'])
+            chlorides             = float(request.form['chlorides'])
+            free_sulphur_dioxide  = float(request.form['free_sulphur_dioxide'])
+            total_sulphur_dioxide = float(request.form['total_sulphur_dioxide'])
+            density               = float(request.form['density'])
+            ph                    = float(request.form['ph'])
+            sulphates             = float(request.form['sulphates'])
+            alcohol               = float(request.form['alcohol'])
+        except ValueError:
+            error_message = 'Invalid input. Please enter valid numbers.'
+            return render_template('error.html', error_message=error_message)
+
         #load the pickle file
         filename = 'random_model.pickle'
         loaded_model = pickle.load(open(filename,'rb'))
@@ -36,6 +41,7 @@ def predict():
         my_prediction = loaded_model.predict(data)
         #get the result template
         return render_template('result.html',prediction=my_prediction)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
